c3d5b9c58d0ffebdf48581d1bf6a4b41ff62f26b
vandhanchowdhary <vandhanchowdhary@gmail.com>
2025-08-11 17:34:35 +0530

    Update relative links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import ChatList from "./pages/ChatList";
 import ChatWindow from "./pages/ChatWindow";
